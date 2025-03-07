d7dc0d3bf734763bd48ea11ece0dc5112a46e85d
Surve <surve8729@gmail.com>
2025-03-07 13:56:09 +0530

    Done with task 4

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
 const myLibrary = [];
 
-function Book(title, author, pages) {
+function Book(title, author, pages, read) {
   this.id = crypto.randomUUID();
   this.title = title;
   this.author = author;
   this.pages = pages;
+  this.read = read;
 }
 
-function addBookToLibrary(title, author, pages) {
-  const newBook = new Book(title, author, pages);
+function addBookToLibrary(title, author, pages, read) {
+  const newBook = new Book(title, author, pages, read);
   myLibrary.push(newBook);
+  displayBooks();
 }
 
-hollow = addBookToLibrary("hollow", "sagar", 123);
-behind = addBookToLibrary("behind", "sahil", 823);
-
-// console.log(myLibrary);
+// hollow = addBookToLibrary("hollow", "sagar", 123);
+// behind = addBookToLibrary("behind", "sahil", 823);
 
 function displayBooks() {
   const booksContainer = document.getElementById("books");
+  booksContainer.innerText = "";
 
   myLibrary.forEach((book) => {
     const card = document.createElement("div");
@@ -27,6 +28,7 @@ function displayBooks() {
     card.innerHTML = `<h3>${book.title}</h3>
       <p><strong>Author:</strong> ${book.author}</p>
       <p><strong>Pages:</strong> ${book.pages}</p>
+      <p><strong>Read:</strong> ${book.read ? "Yes" : "No"}</p>
       <hr>
       `;
 
@@ -35,4 +37,25 @@ function displayBooks() {
   });
 }
 
-document.addEventListener("DOMContentLoaded", displayBooks)
+document.addEventListener("DOMContentLoaded", () => {
+  const newBookBtn = document.getElementById("newBookBtn");
+  const dialog = document.getElementById("bookFormDialog");
+  const closeFormBtn = document.getElementById("closeFormBtn");
+  const form = document.getElementById("bookForm");
+
+  newBookBtn.addEventListener("click", () => dialog.showModal());
+  closeFormBtn.addEventListener("click", () => dialog.close());
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const title = document.getElementById("title").value;
+    const author = document.getElementById("author").value;
+    const pages = document.getElementById("pages").value;
+    const read = document.getElementById("read").value;
+    
+    form.reset();
+    addBookToLibrary(title, author, pages, read);
+    dialog.close();
+  });
+});
