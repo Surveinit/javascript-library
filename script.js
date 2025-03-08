130ab161ea39482684f26b590923dbff3f3b27ce
Surve <surve8729@gmail.com>
2025-03-08 17:53:21 +0530

    ALl done

--- script.js
+++ script.js
@@ -8,14 +8,24 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
+Book.prototype.toggleRead = function () {
+  this.read = !this.read;
+  displayBooks();
+};
+
 function addBookToLibrary(title, author, pages, read) {
   const newBook = new Book(title, author, pages, read);
   myLibrary.push(newBook);
   displayBooks();
 }
 
-// hollow = addBookToLibrary("hollow", "sagar", 123);
-// behind = addBookToLibrary("behind", "sahil", 823);
+function removeBook(bookId) {
+  const index = myLibrary.findIndex((book) => book.id == bookId);
+  if (index !== -1) {
+    myLibrary.splice(index, 1);
+    displayBooks();
+  }
+};
 
 function displayBooks() {
   const booksContainer = document.getElementById("books");
@@ -24,16 +34,27 @@ function displayBooks() {
   myLibrary.forEach((book) => {
     const card = document.createElement("div");
     card.classList.add("card");
-
+    card.dataset.id = book.id;
     card.innerHTML = `<h3>${book.title}</h3>
       <p><strong>Author:</strong> ${book.author}</p>
       <p><strong>Pages:</strong> ${book.pages}</p>
       <p><strong>Read:</strong> ${book.read ? "Yes" : "No"}</p>
+      <button class="toggle-read">Toggle Read</button>
+      <button class="remove-book">Remove</button>
       <hr>
       `;
 
+    // Add event listeners
+    card.querySelector(".toggle-read").addEventListener("click", () => {
+      const bookObj = myLibrary.find((b) => b.id === book.id);
+      bookObj.toggleRead();
+    });
+
+    card.querySelector(".remove-book").addEventListener("click", () => {
+      removeBook(book.id);
+    });
+    
     booksContainer.appendChild(card);
-    console.log(book);
   });
 }
 
